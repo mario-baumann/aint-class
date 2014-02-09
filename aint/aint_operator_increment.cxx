54fea9957f8f4a9fc084568bb6da9b318ab5da50
Mario Baumann <mario-baumann@web.de>
2014-02-09 20:36:08 +0000

    added increment and decrement operators

---
+++ aint/aint_operator_increment.cxx
@@ -0,0 +1,61 @@
+/*
+ *  This file is part of aint.
+ *
+ *  aint provides a C++ class for arbitrary long integers which can be
+ *  used the same way as the C/C++ integer types (e.g. int or long),
+ *  but has no restriction on the length (if there is enough memory).
+ *  Aim of this project is to include fast integer multiplications
+ *  like the Toom-Cook algorithm or the Schoenhage-Strassen algorithm.
+ *  Copyright (C) 2014 Mario Baumann (mario-baumann@web.de)
+ *
+ *  aint is free software: you can redistribute it and/or modify it
+ *  under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  aint is distributed in the hope that it will be useful, but
+ *  WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *  General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with aint.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ **************************************************************************/
+
+// arbitrary long integer
+// aint_operator_increment.cxx (increment operators)
+
+#include "impl.hxx"
+
+namespace astd {
+
+    aint& aint::operator++() {
+        if (this->impl_->positive_) {
+            // aint is positvie
+            if (this->impl_->data_[0] == this->impl_->C_SINGLE_COMP_MAX_P1 - 1) {
+                // handle overflow in last data segment
+                this->impl_->operatorPlus(aint(*this), aint(1), *this);
+            } else {
+                this->impl_->data_[0] += 1;
+            }
+        } else {
+            // aint is negative
+            if (this->impl_->data_[0] == 0) {
+                // handle underflow in last data segment
+                this->impl_->operatorPlus(aint(*this), aint(1), *this, true);
+            } else {
+                this->impl_->data_[0] -= 1;
+                this->impl_->makeZeroPositive();
+            }
+        }
+        return(*this);
+    }
+
+    const aint aint::operator++(int) {
+        aint res(*this); // store current state
+        ++(*this);       // apply above operator
+        return(res);     // return saved state
+    }
+
+} // end of namespace astd
---
+++ aint/cppunit/cppunit_operator_decrement.cxx
@@ -0,0 +1,54 @@
+/*
+ *  This file is part of aint.
+ *
+ *  aint provides a C++ class for arbitrary long integers which can be
+ *  used the same way as the C/C++ integer types (e.g. int or long),
+ *  but has no restriction on the length (if there is enough memory).
+ *  Aim of this project is to include fast integer multiplications
+ *  like the Toom-Cook algorithm or the Schoenhage-Strassen algorithm.
+ *  Copyright (C) 2014 Mario Baumann (mario-baumann@web.de)
+ *
+ *  aint is free software: you can redistribute it and/or modify it
+ *  under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  aint is distributed in the hope that it will be useful, but
+ *  WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *  General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with aint.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ **************************************************************************/
+
+// aint unit tests
+#include "cppunit.hxx"
+
+void aintTest::testOperatorDecrement() {
+
+    CPPUNIT_ASSERT( --aint(   "-1") ==    -2 );
+    CPPUNIT_ASSERT( --aint(    "0") ==    -1 );
+    CPPUNIT_ASSERT( --aint(    "1") ==     0 );
+    CPPUNIT_ASSERT( --aint( "9999") ==  9998 );
+    CPPUNIT_ASSERT( --aint("10000") ==  9999 );
+    CPPUNIT_ASSERT( --aint("10001") == 10000 );
+
+    CPPUNIT_ASSERT( aint(   "-1")-- ==    -1 );
+    CPPUNIT_ASSERT( aint(    "0")-- ==     0 );
+    CPPUNIT_ASSERT( aint(    "1")-- ==     1 );
+    CPPUNIT_ASSERT( aint( "9999")-- ==  9999 );
+    CPPUNIT_ASSERT( aint("10000")-- == 10000 );
+    CPPUNIT_ASSERT( aint("10001")-- == 10001 );
+
+    *a1 = 10000;
+    *a2 = 10000;
+    for (int j = 0; j < 10000; ++j, --(*a1), (*a2)--) {
+        ;
+    }
+    CPPUNIT_ASSERT( *a1 == 0 );
+    CPPUNIT_ASSERT( *a2 == 0 );
+}
+
+// eof
