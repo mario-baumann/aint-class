d51f8fd8c0ba9c5c9234f09d4e48bc3867f4b7d0
Mario Baumann <mario-baumann@web.de>
2018-03-01 21:02:26 +0000

    added exported symbol handling for darwin

--- aint/example/build.sh
+++ aint/example/build.sh
@@ -15,7 +15,7 @@ function build_mingw
     echo ${DLL_PATH_MANGLE} | awk -v d=\" '{ print "LIBRARY " d $1 d }' > a.def
     cat ../../../lib/aint.def >> a.def
     ${DLLTOOL} --output-lib a.lib --input-def a.def
-    ${CXX} -o a.exe ${SOURCE} -I ../../../include a.lib -static-libgcc ${CXX_LIB} ${PTH_LIB}
+    ${CXX} -o a.exe ${SOURCE} -std=c++11 -I ../../../include a.lib -static-libgcc ${CXX_LIB} ${PTH_LIB}
     cat a.exe | sed "s@${DLL_PATH_MANGLE}@${DLL_PATH_DOS}@" > ${OUTPUT}
     chmod 755 ${OUTPUT}
     rm -f a.exe a.lib a.def
@@ -28,8 +28,8 @@ function build
 
     echo "building $biny ..."
     case $(uname -s) in
-    Linux)
-        g++ -o $biny $file -I ../../../include -L ../../../lib -laint -Wl,-rpath,../../../lib
+    Linux|Darwin)
+        g++ -o $biny $file -std=c++11 -I ../../../include -L ../../../lib -laint -Wl,-rpath,../../../lib
         ;;
     CYGWIN*)
         if [ "_$(which i686-w64-mingw32-g++.exe 2> /dev/null)" != "_" ]; then
